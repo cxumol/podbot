d4ee915fc37fcd69b21faf6e96b53f7f30e9abbe
cxumol <cxumol@163.com>
2018-06-28 21:45:44 +0000

    can del and import opml

--- main.go
+++ main.go
@@ -12,14 +12,32 @@ import (
 	"log"
 
 	"encoding/json"
+	"encoding/xml"
 	"strings"
 	"time"
+
+	"net/http"
 
 	"unicode/utf8"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
 
+// to phrase xml
+
+type Pcasts struct {
+	Feeds []feed `xml:"body>outline>outline"`
+}
+
+type feed struct {
+	Type string `xml:"type,attr"`
+	Text string `xml:"text,attr"`
+	Url  string `xml:"xmlUrl,attr"`
+}
+
+// lmx
+
+// handle configue file
 type Config struct {
 	BotApi  string  `json:"BotApi"`
 	AdminID []int64 `json:"AdminID"`
@@ -55,7 +73,7 @@ func loadtgbot(botApi string) (*tgbotapi.BotAPI, tgbotapi.UpdatesChannel) {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -111,12 +129,19 @@ func botHandler(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, db *sql.D
 				}
 			case "help":
 				msg.Text = "Sorry I can't help you. \nYou have to help yourself."
+			case "delete":
+				{
+					if int64InSlice(int64(update.Message.From.ID), config.AdminID) {
+						_, affectedrows := delItem(update.Message.Text, db)
+						msg.Text = fmt.Sprintf("删除了%v条记录", affectedrows)
+					}
+				}
 			default:
 				msg.Text = "use /help to ask someone help you"
 			}
 			msg.ParseMode = tgbotapi.ModeMarkdown
 			bot.Send(msg)
-		} else {
+		} else if update.Message.Text != `` {
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			msg.ReplyToMessageID = update.Message.MessageID
@@ -126,6 +151,25 @@ func botHandler(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, db *sql.D
 			}
 			msg.ParseMode = tgbotapi.ModeMarkdown
 			bot.Send(msg)
+		} else if doc := update.Message.Document; doc != nil {
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "开始解析文件, 请耐心等待")
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			log.Printf("found a file: %v\n", doc.FileName)
+			pendingMsg, _ := bot.Send(msg)
+			var updateMsgText string
+
+			if strings.HasSuffix(doc.FileName, "xml") {
+				fileURL, err := bot.GetFileDirectURL(doc.FileID)
+				chErr(err)
+				updateMsgText = opmlRead(fileURL, db)
+			} else {
+				updateMsgText = "只接受xml文件"
+			}
+
+			editedmsg := tgbotapi.NewEditMessageText(pendingMsg.Chat.ID, pendingMsg.MessageID, updateMsgText)
+			bot.Send(editedmsg)
 		}
 	}
 }
@@ -155,12 +199,42 @@ func addItem(inp string, db *sql.DB) (string, int64) {
 	query, err := db.Prepare("INSERT INTO podcast (name, url, created) VALUES(?,?,?)")
 	chErr(err)
 
+	// tx, err := db.Begin()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
 	resp, err := query.Exec(newRec[0], newRec[1], time.Now().Unix())
 	chErr(err)
+	// resp, err := tx.Stmt(query).Exec(newRec[0], newRec[1], time.Now().Unix())
+	// if err != nil {
+	// 	fmt.Println("doing rollback")
+	// 	tx.Rollback()
+	// } else {
+	// 	tx.Commit()
+	// }
 
 	newid, _ := resp.LastInsertId()
 
 	return newRec[0], newid
+}
+
+func delItem(inp string, db *sql.DB) (string, int64) {
+	// parse input
+	begin_idx := strings.Index(inp, " ")
+	que := fmt.Sprint("DELETE FROM podcast where name like '%", inp[begin_idx+1:], "%'")
+
+	log.Println("deleting: " + inp[begin_idx+1:])
+
+	query, err := db.Prepare(que)
+	chErr(err)
+
+	resp, err := query.Exec()
+	chErr(err)
+
+	affect, _ := resp.RowsAffected()
+
+	return "deleted", affect
 }
 
 func searchUrl(inp string, db *sql.DB) bool {
@@ -169,6 +243,7 @@ func searchUrl(inp string, db *sql.DB) bool {
 	rows, err := db.Query(que)
 	chErr(err)
 
+	defer rows.Close()
 	for rows.Next() {
 		var name string
 		var url string = ""
@@ -189,6 +264,7 @@ func searchItem(inp string, db *sql.DB) string {
 	chErr(err)
 
 	result := ``
+	defer rows.Close()
 	for rows.Next() {
 		var name string
 		var url string
@@ -200,6 +276,43 @@ func searchItem(inp string, db *sql.DB) string {
 	return result
 }
 
+func opmlRead(opmlURL string, db *sql.DB) string {
+	// newtmp()
+	resp, err := http.Get(opmlURL)
+	chErr(err)
+	bxml, err := ioutil.ReadAll(resp.Body)
+	chErr(err)
+
+	urfeeds := Pcasts{}
+	err = xml.Unmarshal(bxml, &urfeeds)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		//return
+	}
+
+	// 读取完毕
+	log.Println(urfeeds)
+	if len(urfeeds.Feeds) == 0 {
+		return "只支持 Pocket Casts 导出的文件"
+	}
+	// 开始导入
+	for _, feed := range urfeeds.Feeds {
+		if feed.Type == "rss" {
+
+			inp := fmt.Sprintf("_add %v %v", feed.Text, feed.Url)
+			chErr(err)
+
+			itemName, reternID := addItem(inp, db)
+			if reternID > 0 {
+				log.Printf("登记 %s 于 %v", itemName, reternID)
+			}
+
+		}
+	}
+	return "导入成功"
+
+}
+
 func dbErr(err error) {
 	if err != nil {
 		println("You need initialize sql.db at first.")
@@ -209,7 +322,7 @@ func dbErr(err error) {
 
 func chErr(err error) {
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 }
 
@@ -221,3 +334,32 @@ func int64InSlice(a int64, list []int64) bool {
 	}
 	return false
 }
+
+// func PathExists(path string) (bool, error) {
+// 	_, err := os.Stat(path)
+// 	if err == nil {
+// 		return true, nil
+// 	}
+// 	if os.IsNotExist(err) {
+// 		return false, nil
+// 	}
+// 	return false, err
+// }
+
+// func newtmp() {
+// 	_dir := "./tmp"
+// 	exist, err := PathExists(_dir)
+// 	if err != nil {
+// 		log.Printf("get dir error![%v]\n", err)
+// 		return
+// 	}
+
+// 	if !exist {
+// 		err := os.Mkdir(_dir, os.ModePerm)
+// 		if err != nil {
+// 			log.Printf("mkdir failed![%v]\n", err)
+// 		} else {
+// 			log.Printf("mkdir success!\n")
+// 		}
+// 	}
+// }
